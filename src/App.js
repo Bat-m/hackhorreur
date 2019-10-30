a241796b0992193d7b4c6b0091497ec18d46e1ba
Corinne Russo <corinne.russo@gmail.com>
2019-10-30 14:34:10 +0100

    home with image and name and button play

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import GetPost from './components/GetPost';
+// import GetPost from './components/GetPost';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       {/* <GetPost/> */}
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import spaceBackground from './../../src/assets/spaceBackground.jpg';
 
 export default class Home extends React.Component{
+    render() {
+        return (
+          <div className="home">
+            <img className="home-background" src={spaceBackground} alt="space environment"></img>
+            <h1>SAINT GROOL STORIE</h1>
+            <button> PLAY </button>
+          </div>
+        )
+      }
     
 }
