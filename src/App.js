481cb375c7e7c3881eefaf5d18454a7dd5213c8f
mrfouine <kas3rkin@gmail.com>
2019-10-30 14:17:40 +0100

    bat add route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 import GetPost from './components/GetPost';
 
+import Charac from './components/Character/HomeCharacter';
+import Stories from './components/Stories/Stories';
+
+
 function App() {
   return (
     <div className="App">
       {/* <GetPost/> */}
+      <Switch>
+        <Route exact path="/" component={Charac}/>
+        <Route path="/stories" component={Stories}/>
+          
+      </Switch>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,4 +2,14 @@ import React from 'react';
 
 export default class Home extends React.Component{
     
+    render(){
+        return(
+            <div className="Home">
+                
+            <button><Link to='/'>Play</Link></button>
+
+            </div>
+        )
+
+    }
 }
