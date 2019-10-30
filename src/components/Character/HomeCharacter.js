12e6b00307f273c4f9993e3719e5bbc305850876
mrfouine <kas3rkin@gmail.com>
2019-10-30 20:07:44 +0100

    add some function inside HomeCharacter.js

--- src/components/Character/HomeCharacter.js
+++ src/components/Character/HomeCharacter.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Character from "./Character";
 import Axios from "axios";
+
 import'./HomeCharacter.css'
 
 export default class HomeCharacter extends React.Component {
   constructor(props) {
     super(props);
+    
     this.state = {
-      heroesAll: []
+      heroesAll: [],
+      
     };
+    
   }
-
+  
   componentDidMount() {
     Axios.get("http://192.168.1.145:8000/characters/everyone")
       .then(response => response.data)
@@ -22,15 +26,17 @@ export default class HomeCharacter extends React.Component {
   }
 
   render() {
-    console.log(this.state.heroesAll[0]);
+ 
     return (
       <div className="HomeCharacters">
         <h1>Choix perso</h1>
         <div className="HomeCharacters-card">
           {this.state.heroesAll.map((item, index) => (
-            <Character key={index} heroes={item} image="" />
+            <Character key={index} heroes={item} image="" ></Character>
+            
           ))}
         </div>
+        
       </div>
     );
   }
