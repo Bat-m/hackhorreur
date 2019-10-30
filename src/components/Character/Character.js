2e54f61db1cc362f2f74f9700ce5f099dfb30818
mrfouine <kas3rkin@gmail.com>
2019-10-30 20:06:40 +0100

    add some function inside character.js

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,23 +1,52 @@
 import React from "react";
 import './Character.css';
+import {Link} from 'react-router-dom';
 
-export default function Character({ heroes }) {
-  return (
-    <div className="Characters">
-      <div className="characters-card">
-        <img src={heroes.image} alt={heroes.image} />
-        <div className="character-contain">
-          <h3>
-            <b>{heroes.name}</b>
-          </h3>
-          <p>PV: {heroes.HP}</p>
-          <p>Vitesse: {heroes.agility}</p>
-          <p>Attaque: {heroes.atk}</p>
-          <p>Defense: {heroes.def}</p>
-          <p>Description: {heroes.description}</p>
-          <p>Origine: {heroes.origin}</p>
+export default class Character extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      heroesAll: [],
+      
+    };
+    
+  }
+
+  onChange = event => {
+    const test=this.props.heroes
+    localStorage.setItem('myValueInLocalStorage', JSON.stringify(test));
+  };
+
+  onButton = () => {
+    localStorage.clear();
+  };
+
+  render(){
+    
+    // console.log(this.props.heroes)
+    return (
+      <div className="Characters">
+        <div className="characters-card">
+          <img src={this.props.heroes.image} alt={this.props.heroes.image} />
+          <div className="character-contain">
+            <h3>
+              <b>{this.props.heroes.name}</b>
+            </h3>
+            <p>PV: {this.props.heroes.HP}</p>
+            <p>Vitesse: {this.props.heroes.agility}</p>
+            <p>Attaque: {this.props.heroes.atk}</p>
+            <p>Defense: {this.props.heroes.def}</p>
+            <p>Description: {this.props.heroes.description}</p>
+            <p>Origine: {this.props.heroes.origin}</p>
+          </div>
+          <button onClick={this.onButton}>Reset localstorage</button>
+          {/* <button onClick={this.onChange}>test localStorage</button> */}
+          <Link to='/stories' onClick={this.onChange} className="">Je veux toi</Link>
         </div>
       </div>
-    </div>
-  );
+    );
+
+  }
+  
 }
