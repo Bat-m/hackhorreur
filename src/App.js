693cba665b568ee4159bba3531ba28942767711f
mrfouine <kas3rkin@gmail.com>
2019-10-30 14:03:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import Forms from './components/Forms';
-
 import GetPost from './components/GetPost';
 
 function App() {
   return (
     <div className="App">
       {/* <GetPost/> */}
-      <Forms/>
     </div>
   );
 }
