70a972c36a0fe863f23262fc1db02598f0693f53
Corinne Russo <corinne.russo@gmail.com>
2019-10-30 18:39:59 +0100

    first version of cards

--- src/components/Character/HomeCharacter.js
+++ src/components/Character/HomeCharacter.js
@@ -2,6 +2,7 @@ import React from "react";
 import Character from "./Character";
 import Axios from "axios";
 import'./HomeCharacter.css'
+import bandeau from './../../assets/bandeau.jpg'
 
 export default class HomeCharacter extends React.Component {
   constructor(props) {
@@ -25,7 +26,11 @@ export default class HomeCharacter extends React.Component {
     console.log(this.state.heroesAll[0]);
     return (
       <div className="HomeCharacters">
-        <h1>Choix perso</h1>
+        <h1>Quel horror héros seras-tu ?</h1>
+
+        <p>Ta planète a été percutée par un astéroïde et tu t'es <i>légèrement</i> transformé(e). 
+          Tu as réussi à monter dans une capsule partant vers un autre monde ... Tu attéris sur une nouvelle planète mais tu ne sais pas si elle est habitée!</p>
+
         <div className="HomeCharacters-card">
           {this.state.heroesAll.map((item, index) => (
             <Character key={index} heroes={item} image="" />
