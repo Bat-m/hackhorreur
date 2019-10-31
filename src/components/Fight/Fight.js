54bee4751aa0680bb792e66d708153cdc5241ef9
mrfouine <kas3rkin@gmail.com>
2019-10-31 02:29:07 +0100

    update home.js

--- src/components/Fight/Fight.js
+++ src/components/Fight/Fight.js
@@ -1,57 +1,59 @@
-import React from 'react';
-import Axios from 'axios';
-import {Link} from 'react-router-dom';
-import './../Fight/Fight.css';
-import poing from './../../assets/poing2.png';
-import bouclier from './../../assets/bouclier.jpg';
-import empreinte from'./../../assets/fuir.png'
+import React from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+import "./../Fight/Fight.css";
+import poing from "./../../assets/poing2.png";
+import bouclier from "./../../assets/bouclier.jpg";
+import empreinte from "./../../assets/fuir.png";
 
 export default class Fight extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          hero: []
-        };
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hero: []
+    };
+  }
 
-    
-    //   componentDidMount() {
-    //     Axios.get(process.env.REACT_APP_RICHARD_IP + "/characters/hero")
-    //       .then(response => response.data)
-    //       .then(data =>  this.setState({
-    //         hero: data})
-    //       );
-    //   }
+  //   componentDidMount() {
+  //     Axios.get(process.env.REACT_APP_RICHARD_IP + "/characters/hero")
+  //       .then(response => response.data)
+  //       .then(data =>  this.setState({
+  //         hero: data})
+  //       );
+  //   }
 
-    //   onChange = event => {
-    //     // const [value, setValue] = React.useState('');
-    //     // localStorage.getItem('myValueInLocalStorage')
-    //     // console.log(localStorage.getItem('myValueInLocalStorage'))
-    //   };
+  //   onChange = event => {
+  //     // const [value, setValue] = React.useState('');
+  //     // localStorage.getItem('myValueInLocalStorage')
+  //     // console.log(localStorage.getItem('myValueInLocalStorage'))
+  //   };
 
-    render(){
-        return(
-            <div >
-                <h1 className="fightZone">FIGTH ZONE</h1>
-                
-                {/* <img src={this.props.heroes.image} alt={this.props.heroes.image} /> */}
-                
-                <div className="fightZone-defense">
-                <div className="fightZone-btn">Attaquer</div>
-                <img className="fightZone-poing" src={poing} alt ="poing"></img>
-                </div>
+  render() {
+    return (
+      <div>
+        <h1 className="fightZone">FIGTH ZONE</h1>
 
-                <div className="fightZone-defense">
-                <div className="fightZone-btn">Défendre</div>
-                <img className="fightZone-poing" src={bouclier} alt ="bouclier"></img>
-                </div>
-                <div className="fightZone-defense">
-                <div className="fightZone-btn">Fuir</div>
-                <img className="fightZone-poing" src={empreinte} alt ="empreinte"></img>
-                 {/* <img src={this.props.allHeroes.image} alt={this.props.allHeroes.image} /> */}
-                 </div>
-            </div>
-        )
+        {/* <img src={this.props.heroes.image} alt={this.props.heroes.image} /> */}
 
-    }
+        <div className="fightZone-defense">
+          <div className="fightZone-btn">Attaquer</div>
+          <img className="fightZone-poing" src={poing} alt="poing"></img>
+        </div>
+
+        <div className="fightZone-defense">
+          <div className="fightZone-btn">Défendre</div>
+          <img className="fightZone-poing" src={bouclier} alt="bouclier"></img>
+        </div>
+        <div className="fightZone-defense">
+          <div className="fightZone-btn">Fuir</div>
+          <img
+            className="fightZone-poing"
+            src={empreinte}
+            alt="empreinte"
+          ></img>
+          {/* <img src={this.props.allHeroes.image} alt={this.props.allHeroes.image} /> */}
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import './home.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
 
-export default class Home extends React.Component{
-    render() {
-        return (
-            
-          <div className="home">
-             
-                  <div className="home-title-and-button">
-                  <div class="halloween">
-                      <div className="home-st-grool"> 
-                        <h1 className="home-st-grool-title">SAINT GROOL STORIE</h1>
-                    </div>
-                    <div className="home-play">
-                        <Link to='/charac' className="home-play-button">PLAY !</Link>
-                    </div>
-                  </div>
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="home-title-and-button">
+          <div class="halloween">
+            <div className="home-st-grool">
+              <h1 className="home-st-grool-title">SAINT GROOL STORIE</h1>
+            </div>
+            <div className="home-play">
+              <Link to="/charac" className="home-play-button">
+                PLAY !
+              </Link>
             </div>
           </div>
-          
-        )
-      }
-    
+        </div>
+      </div>
+    );
+  }
 }
